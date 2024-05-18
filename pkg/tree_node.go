98ad9bcffb6b615ae32c1cdb271c1d016d3117bc
xuji <17623396658@163.com>
2024-05-18 16:57:45 +0800

    treeprint commit

--- pkg/tree_node.go
+++ pkg/tree_node.go
@@ -1,15 +1,74 @@
 package pkg
 
+import "fmt"
+
+// 定义二叉树结构
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
 }
 
+// 初始化二叉树结点
 func NewTreeNode(v int) *TreeNode {
 	return &TreeNode{
 		Val:   v,
 		Left:  nil,
 		Right: nil,
 	}
+}
+
+/********************打印二叉树结构，别人实现的****************************************/
+// 打印二叉树结构
+func PrintTree(root *TreeNode) {
+	printTreeHelper(root, nil, false)
+}
+
+// printTreeHelper Help to print a binary tree, hide more details
+// This tree printer is borrowed from TECHIE DELIGHT
+// https://www.techiedelight.com/c-program-print-binary-tree/
+type trunk struct {
+	prev *trunk
+	str  string
+}
+
+func newTrunk(prev *trunk, str string) *trunk {
+	return &trunk{
+		prev: prev,
+		str:  str,
+	}
+}
+
+func showTrunk(t *trunk) {
+	if t == nil {
+		return
+	}
+
+	showTrunk(t.prev)
+	fmt.Print(t.str)
+}
+
+func printTreeHelper(root *TreeNode, prev *trunk, isLeft bool) {
+	if root == nil {
+		return
+	}
+	prevStr := "    "
+	trunk := newTrunk(prev, prevStr)
+	printTreeHelper(root.Right, trunk, true)
+	if prev == nil {
+		trunk.str = "———"
+	} else if isLeft {
+		trunk.str = "/———"
+		prevStr = "   |"
+	} else {
+		trunk.str = "\\———"
+		prev.str = prevStr
+	}
+	showTrunk(trunk)
+	fmt.Println(root.Val)
+	if prev != nil {
+		prev.str = prevStr
+	}
+	trunk.str = "   |"
+	printTreeHelper(root.Left, trunk, false)
 }
